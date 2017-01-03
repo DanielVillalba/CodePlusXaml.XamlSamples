00046e954ae1c7e63329e7ca2087a294f3227bc0
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-01-03 12:17:43 -0600

    Adding an extra Label view from the code behind file

--- CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
+++ CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
@@ -13,6 +13,21 @@ namespace CodePlusXaml
         public CodePlusXamlPage()
         {
             InitializeComponent();
+
+            Label label = new Label
+            {
+                Text = "Hello from Code!",
+                IsVisible = true,
+                Opacity=0.75,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalOptions=LayoutOptions.CenterAndExpand,
+                TextColor=Color.Blue,
+                BackgroundColor=Color.FromRgb(255,128,128),
+                FontSize=Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+
+            (Content as StackLayout).Children.Insert(1, label);
         }
     }
 }
